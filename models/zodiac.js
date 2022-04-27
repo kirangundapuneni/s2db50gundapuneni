a44183a0f212591e7c0d2a153372e3f4e95005c5
Kiran Kumar Gundapuneni <98048115+kirangundapuneni@users.noreply.github.com>
2022-04-27 02:31:14 -0500

    Part 4 to 6

--- models/zodiac.js
+++ models/zodiac.js
@@ -2,14 +2,27 @@
  * @Author: Your name
  * @Date:   2022-04-13 16:43:32
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-04-21 23:04:21
+ * @Last Modified time: 2022-04-27 01:54:35
  */
 const mongoose = require("mongoose")
 new mongoose.Types.ObjectId()
 const zodiacSchema = mongoose.Schema({
-zodiac_name: String,
-zodiac_meaning: String,
-lucky_number: Number
+    zodiac_name: { 
+        type: String,
+        minLength: 3,
+        maxLength: 20, 
+        required: true,
+        trim: true
+    },
+    zodiac_meaning: { 
+        type: String, 
+        required: true
+    },
+    lucky_number: { 
+        type: Number, 
+        min:[0, 'Must be at least 0, got {VALUE}'], 
+        max:9999
+    }
 })
 module.exports = mongoose.model("Zodiac",
 zodiacSchema)
--- routes/zodiacs.js
+++ routes/zodiacs.js
@@ -2,21 +2,29 @@
  * @Author: Your name
  * @Date:   2022-04-08 11:25:29
  * @Last Modified by:   Your name
- * @Last Modified time: 2022-04-21 02:28:13
+ * @Last Modified time: 2022-04-27 02:19:45
  */
 var express = require('express');
 const zodiac_controller= require('../controllers/zodiac');
 var router = express.Router();
 
+const secured = (req, res, next) => {
+    if (req.user) {
+      return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+  }
+
 /* GET Zodiac */
 router.get('/', zodiac_controller.zodiac_view_all );
-/* GET detail car page */
+/* GET detail zodiac page */
 router.get('/detail', zodiac_controller.zodiac_view_one_Page);
 /* GET create update page */
-router.get('/update', zodiac_controller.zodiac_update_Page);
+router.get('/update', secured, zodiac_controller.zodiac_update_Page);
 /* GET create zodiac page */
-router.get('/create', zodiac_controller.zodiac_create_Page);
-/* GET delete car page */
-router.get('/delete', zodiac_controller.zodiac_delete_Page);
+router.get('/create', secured, zodiac_controller.zodiac_create_Page);
+/* GET delete zodiac page */
+router.get('/delete',secured, zodiac_controller.zodiac_delete_Page);
 
 module.exports = router;
